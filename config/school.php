7cd8e796e1ee6ae3886fbdf41847251c07ebeb07
Fuad Hasan <fuadhasan8001@gmail.com>
2026-09-26 17:34:08 +0700

    Configure journal geofence and compact mobile login

--- config/school.php
+++ config/school.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'latitude' => env('SCHOOL_LATITUDE'),
-    'longitude' => env('SCHOOL_LONGITUDE'),
-    'radius_meters' => env('SCHOOL_RADIUS_METERS'),
+    'latitude' => env('SCHOOL_LATITUDE', -8.081940),
+    'longitude' => env('SCHOOL_LONGITUDE', 111.900574),
+    'radius_meters' => env('SCHOOL_RADIUS_METERS', 90),
     'max_gps_accuracy' => env('SCHOOL_MAX_GPS_ACCURACY'),
 ];
